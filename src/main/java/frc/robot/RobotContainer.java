15a0b61dd798447986c14c984313cee29bfccd75
MrDrGru <krahn@sdsmstudents.org>
2023-11-02 16:48:36 -0700

    PlayAudio

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import frc.robot.commands.PlayAudio;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.SwerveJoystickCmd;
@@ -9,27 +10,29 @@ import frc.robot.commands.WritePicture;
 import frc.robot.commands.ZeroHeadingCmd;
 import frc.robot.commands.setPositionA;
 import frc.robot.subsystems.PIDSub;
+import frc.robot.subsystems.PlayAudio;
 import frc.robot.subsystems.SwerveSubsystem;
-
 
 public class RobotContainer {
 
         // Create instance of each Subsystem:
         private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+        private final PlayAudio m_PlayAudio = new PlayAudio();
         private final PIDSub m_PID = new PIDSub();
         // Create instance of each Controller/Joystick:
         private final Joystick driverJoytick = new Joystick(OIConstants.kDriverControllerPort);
+        private final CommandXboxController m_driverController = new CommandXboxController(0);
 
- 
         public RobotContainer() {
 
-        // Define Controller/Joystick mapping within RobotContainer and call "configureButtonBindings()":
+                // Define Controller/Joystick mapping within RobotContainer and call
+                // "configureButtonBindings()":
                 swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(
-                swerveSubsystem,
-                () -> driverJoytick.getRawAxis(OIConstants.kDriverYAxis),
-                () -> driverJoytick.getRawAxis(OIConstants.kDriverXAxis),
-                () -> driverJoytick.getRawAxis(OIConstants.kDriverRotAxis),
-                () -> !driverJoytick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx)));
+                                swerveSubsystem,
+                                () -> driverJoytick.getRawAxis(OIConstants.kDriverYAxis),
+                                () -> driverJoytick.getRawAxis(OIConstants.kDriverXAxis),
+                                () -> driverJoytick.getRawAxis(OIConstants.kDriverRotAxis),
+                                () -> !driverJoytick.getRawButton(OIConstants.kDriverFieldOrientedButtonIdx)));
 
                 configureButtonBindings();
         }
@@ -38,15 +41,14 @@ public class RobotContainer {
         private void configureButtonBindings() {
 
                 new JoystickButton(driverJoytick, 5).onTrue(new ZeroHeadingCmd(swerveSubsystem));
-                //PID Comands Bellow
+                // PID Comands Bellow
                 new JoystickButton(driverJoytick, 3).onTrue(new setPositionA(m_PID, 1));
                 // m_driverController.b().onFalse(new setPositionA(m_PID, 0));
                 new JoystickButton(driverJoytick, 4).onTrue(new setPositionA(m_PID, 2));
                 // m_driverController.a().onFalse(new setPositionA(m_PID, 0));
                 new JoystickButton(driverJoytick, 1).onTrue(new WritePicture());
 
-                new JoystickButton(driverJoytick, 8).onTrue(new PlayAudio());
-
+                m_driverController.a().whileTrue(m_PlayAudio.PressA());
         }
 
         // Run AutonomousCommand:
---
+++ src/main/java/frc/robot/subsystems/PlayAudio.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot.subsystems;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.System.Logger;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PlayAudio extends SubsystemBase {
+
+  Long currentFrame;
+  Clip clip;
+
+  // current status of clip
+  String status;
+
+  AudioInputStream audioInputStream;
+  static String filePath;
+
+  public PlayAudio() {
+    // create AudioInputStream object
+    try {
+      filePath = "C:/Users/oclon/OneDrive/Documents/windows-xp-startup-sound/windows_xp.wav";
+
+      audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+      // create clip reference
+      clip = AudioSystem.getClip();
+
+      // open audioInputStream to the clip
+      clip.open(audioInputStream);
+      play();
+    } catch (Exception e) {
+    }
+  }
+
+  public void play() {
+    // start the clip
+    clip.start();
+
+    status = "play";
+  }
+
+  public void stop() {
+    currentFrame = 0L;
+    clip.stop();
+    clip.close();
+  }
+
+  public CommandBase PressA() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          new PlayAudio();
+          System.out.println("!!!Did you know that PlayAudio actually ran!!!");
+        });
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+}

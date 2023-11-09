93a39e7ea1da1527d519fc33726b75f1a415f0f5
MrDrGru <krahn@sdsmstudents.org>
2023-11-09 15:27:42 -0800

    e

--- src/main/java/frc/robot/subsystems/PlayAudio.java
+++ src/main/java/frc/robot/subsystems/PlayAudio.java
@@ -30,7 +30,7 @@ public class PlayAudio extends SubsystemBase {
   public PlayAudio() {
     // create AudioInputStream object
     try {
-      filePath = "C:/Users/oclon/OneDrive/Documents/GitHubCode/Selfie-Robot/src/main/java/frc/robot/SnapSound.wav";
+      filePath = "src/main/java/frc/robot/SnapSound.wav";
 
       audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
       // create clip reference
